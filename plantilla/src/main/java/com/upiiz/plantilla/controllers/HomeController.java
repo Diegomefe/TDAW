206554e437baab91a5dcb50e966e7d9f255cdd37
Diegomefe <160690670+Diegomefe@users.noreply.github.com>
2025-03-10 20:30:22 -0600

    Add files via upload

---
+++ plantilla/src/main/java/com/upiiz/plantilla/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.upiiz.plantilla.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping ("/elements")
+    public String elements(){
+        return "elements";
+    }
+
+    @GetMapping ("/generic")
+    public String generics(){
+        return "generic";
+    }
+}
