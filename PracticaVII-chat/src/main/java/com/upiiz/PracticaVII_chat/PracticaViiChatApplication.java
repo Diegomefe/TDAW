a53733b1aa8ff0b1a48ef2d214bea0f75027de3d
Diego <jdmf_05@hotmail.com>
2025-06-17 19:01:47 -0600

    Agregar practica7

---
+++ PracticaVII-chat/src/main/java/com/upiiz/PracticaVII_chat/PracticaViiChatApplication.java
@@ -0,0 +1,13 @@
+package com.upiiz.PracticaVII_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaViiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaViiChatApplication.class, args);
+	}
+
+}
